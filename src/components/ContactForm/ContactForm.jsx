5f228ad88cf46e7441eb7486f5bfbb30c182f0bd
Karolina Zaiats <karolina.ivanitska@gmail.com>
2024-04-10 12:20:33 +0200

    configure-form

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,5 +1,30 @@
+import { Field, Form, Formik } from "formik";
+import { nanoid } from "nanoid";
+
 const ContactForm = () => {
-  return <div>Contact Form</div>;
+  const initialValues = {
+    name: "",
+    number: "",
+  };
+  const handleSubmit = (data, options) => {
+    console.log({ ...data, id: nanoid() });
+    options.resetForm();
+  };
+  return (
+    <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+      <Form>
+        <label>
+          Name
+          <Field type="text" name="name" />
+        </label>
+        <label>
+          Number
+          <Field type="text" name="number" />
+        </label>
+        <button type="submit">Add contact</button>
+      </Form>
+    </Formik>
+  );
 };
 
 export default ContactForm;
